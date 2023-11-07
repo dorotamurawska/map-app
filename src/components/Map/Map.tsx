5946459c7ac98eb862f9222d4fa1b1840f4fbfe0
Dorotka <dorotamamczak@gmail.com>
2023-11-07 13:41:47 +0100

    added server for my react app

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,11 +1,33 @@
-import React from 'react';
+import React from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { LatLngBoundsExpression, LatLngExpression } from "leaflet";
-import { MapContainer, TileLayer, Marker, Popup, GeoJSON } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
-import data from '../../geoData/geoJson.json';
-import {GeoJsonObject} from 'geojson';
+import { MapContainer, TileLayer, GeoJSON, useMapEvents, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import { GeoJsonObject } from "geojson";
 
-const Map: React.FC= () => {
+function MyLocationMarker() {
+  const [clickedPosition, setClickedPosition] = useState<LatLngExpression>([0,0]);
+  const map = useMapEvents({
+    click() {
+      map.locate()
+    },
+    locationfound(e) {
+      setClickedPosition(e.latlng)
+      console.log(e.latlng)
+      // map.flyTo(e.latlng, map.getZoom())
+    },
+  })
+
+  return clickedPosition === null ? null : (
+    <Marker position={clickedPosition}>
+      <Popup>You are here</Popup>
+    </Marker>
+  )
+}
+
+
+const Map: React.FC = () => {
   // Default coordinates set to Warsow central station
   const position: LatLngExpression = [52.24938160480759, 21.013992440543763];
   const initialZoom: number = 13;
@@ -16,27 +38,49 @@ const Map: React.FC= () => {
   const northEast: LatLngExpression = [52.429325, 21.459572];
   const customerMaxBounds: LatLngBoundsExpression = [southWest, northEast];
 
+  const [dzielnica, setDzielnica] = useState<GeoJsonObject | null>(null);
+  // const [clickedPosition, setClickedPosition] = useState<LatLngExpression>();
+
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get("/api");
+      setDzielnica(response.data);
+      console.log("Odpowiedź z serwera:", response.data as GeoJsonObject);
+      console.log(dzielnica);
+    } catch (error) {
+      console.error("Błąd żądania:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const geoJSONStyle = {
+    color: "green", // Kolor linii
+    weight: 2, // Grubość linii
+    fillOpacity: 0.1, // Przezroczystość wypełnienia
+  };
 
   return (
     <div>
-      <MapContainer 
-      center={position} 
-      zoom={initialZoom} 
-      scrollWheelZoom={true}
-      doubleClickZoom={false}
-      minZoom={minZoom} 
-      maxBounds={customerMaxBounds}
+      <MapContainer
+        center={position}
+        zoom={initialZoom}
+        scrollWheelZoom={true}
+        doubleClickZoom={false}
+        minZoom={minZoom}
+        maxBounds={customerMaxBounds}
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={position}>
-          <Popup>
-            A pretty CSS3 popup.
-          </Popup>
-        </Marker>
-        <GeoJSON data={data as GeoJsonObject} />
+        {dzielnica && (
+          <GeoJSON data={dzielnica as GeoJsonObject} style={geoJSONStyle} />
+        )}
+        <MyLocationMarker/>
       </MapContainer>
     </div>
   );
